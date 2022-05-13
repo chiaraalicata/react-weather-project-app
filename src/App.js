94e0bab5bc732209c4ab56d48179e272ced02dd0
chiaraalicata <74967879+chiaraalicata@users.noreply.github.com>
2022-05-13 13:17:55 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/chiara-alicata-9b0529177/"
-          target="_blank"
-        >
-          Chiara Alicata
-        </a>{" "}
-        and it is open-sourced on{" "}
-        <a
-          href="https://github.com/chiaraalicata/react-weather-project-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/chiara-alicata-9b0529177/"
+            target="_blank"
+          >
+            Chiara Alicata
+          </a>{" "}
+          and it is open-sourced on{" "}
+          <a
+            href="https://github.com/chiaraalicata/react-weather-project-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
